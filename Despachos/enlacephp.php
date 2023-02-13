159fb5f2d21f3e6aed503a7dcfbedbacb1fd0e3e
OscarCaicedo30 <ecologiasegundousta@gmail.com>
2023-02-13 18:52:00 -0500

    Despachos

---
+++ Despachos/enlacephp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operaciones</title>
+</head>
+<body>
+    <?php
+
+    $descripcion=$_POST['a'];
+    $salario=$_POST['b'];
+    $usuarios=$_POST['c'];
+
+    $conexion=mysqli_connect("localhost","root","","mueblesdecasa") or 
+    die ("Problemas en la conexion");
+
+    mysqli_query($conexion,"insert into tabladespachos(FECHADESPACHO,IDFACTURA,OBSERVACIONES) values ('$descripcion','$salario','$usuarios')") or 
+    die("Problemas en el select".mysqli_error($conexion));
+
+    mysqli_close($conexion);
+
+    echo"Cargo insertado con exito";
+    echo "<table border='2' width='80%'><tr><th>FECHA DESPACHO</th><th>ID FACTURA</th><th>OBSERVACIONES</th></tr>";
+    echo "<table border='2' width='80%'><tr><th>$descripcion</th><th>$salario</th><th>$usuarios</th></tr>";
+    
+    
+    
+    ?>
+</body>
+</html>
